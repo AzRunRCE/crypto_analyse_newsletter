0c85f2396c589d920ec89b5ce5eeacede75ac9e1
AzzRun <qmartine@student.42.fr>
2018-01-22 22:20:44 +0100

    clean

--- crypto_analyse_newsletter/Program.cs
+++ crypto_analyse_newsletter/Program.cs
@@ -12,34 +12,27 @@ namespace crypto_analyse_newsletter
         static void Main(string[] args)
         {
             FeedParser feedParser = new FeedParser();
-
-
-
             while (true)
             {
                 List<string> ArticleKnowed = GetArticlesHash();
                 IList<Item> items = feedParser.Parse("https://crypto-analyse.org/category/accueil/analyses/feed/", FeedType.RSS).Where(x => ArticleKnowed.Contains(x.Guid) == false).ToList();
                 foreach (var itm in items)
                 {
-
                     using (WebClient wc = new WebClient())
                     {
                         string htmlString = wc.DownloadString(itm.Link);
                         HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
                         htmlDocument.LoadHtml(htmlString);
-
                         var text = htmlDocument.DocumentNode.SelectSingleNode("//*[@id='off-canvas-body']/div[4]/div/div/div/div[1]/div/article").InnerHtml;
                         var msg = new MailMessage("crypto-analyse@crypto.com", "quentin.martinez@outlook.com", itm.Title, text);
                         msg.To.Add("quentin.martinez@outlook.com");
                         msg.To.Add("bc@chunkz.net");
-
                         msg.IsBodyHtml = true;
                         var smtpClient = new SmtpClient("smtp.free.fr", 25); //if your from email address is "from@hotmail.com" then host should be "smtp.hotmail.com"**
                         smtpClient.UseDefaultCredentials = true;
                         smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
                         smtpClient.EnableSsl = true;
                         smtpClient.Send(msg);
-                     
                         Console.WriteLine(itm.Title + " Email Sended Successfully");
                     }
 
@@ -47,7 +40,6 @@ namespace crypto_analyse_newsletter
                 }
                 Console.Clear();
                 Console.WriteLine("Scanned at " + DateTime.Now.ToString());
-                
                 System.Threading.Thread.Sleep(1500);
             }
 
