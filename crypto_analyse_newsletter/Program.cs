f2e21d3c5af3e3b063e4ad42d923df67db663362
AzzRun <qmartine@student.42.fr>
2018-01-23 20:59:31 +0100

    fix sometime crash

--- crypto_analyse_newsletter/Program.cs
+++ crypto_analyse_newsletter/Program.cs
@@ -14,33 +14,41 @@ namespace crypto_analyse_newsletter
             FeedParser feedParser = new FeedParser();
             while (true)
             {
-                List<string> ArticleKnowed = GetArticlesHash();
-                IList<Item> items = feedParser.Parse("https://crypto-analyse.org/category/accueil/analyses/feed/", FeedType.RSS).Where(x => ArticleKnowed.Contains(x.Guid) == false).ToList();
-                foreach (var itm in items)
+                try
                 {
-                    using (WebClient wc = new WebClient())
+                    string ArticleKnowed = GetArticlesHash();
+                    IList<Item> items = feedParser.Parse("https://crypto-analyse.org/category/accueil/analyses/feed/", FeedType.RSS).Where(x => ArticleKnowed.Contains(x.Guid) == false).ToList();
+                    foreach (var itm in items)
                     {
-                        string htmlString = wc.DownloadString(itm.Link);
-                        HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
-                        htmlDocument.LoadHtml(htmlString);
-                        var text = htmlDocument.DocumentNode.SelectSingleNode("//*[@id='off-canvas-body']/div[4]/div/div/div/div[1]/div/article").InnerHtml;
-                        var msg = new MailMessage("crypto-analyse@crypto.com", "quentin.martinez@outlook.com", itm.Title, text);
-                        msg.To.Add("quentin.martinez@outlook.com");
-                        msg.To.Add("bc@chunkz.net");
-                        msg.IsBodyHtml = true;
-                        var smtpClient = new SmtpClient("smtp.free.fr", 25); //if your from email address is "from@hotmail.com" then host should be "smtp.hotmail.com"**
-                        smtpClient.UseDefaultCredentials = true;
-                        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                        smtpClient.EnableSsl = true;
-                        smtpClient.Send(msg);
-                        Console.WriteLine(itm.Title + " Email Sended Successfully");
-                    }
+                        using (WebClient wc = new WebClient())
+                        {
+                            string htmlString = wc.DownloadString(itm.Link);
+                            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                            htmlDocument.LoadHtml(htmlString);
+                            var text = htmlDocument.DocumentNode.SelectSingleNode("//*[@id='off-canvas-body']/div[4]/div/div/div/div[1]/div/article").InnerHtml;
+                            var msg = new MailMessage("crypto-analyse@crypto.com", "quentin.martinez@outlook.com", itm.Title, text);
+                            msg.To.Add("quentin.martinez@outlook.com");
+                            msg.To.Add("bc@chunkz.net");
+                            msg.IsBodyHtml = true;
+                            var smtpClient = new SmtpClient("smtp.free.fr", 25); //if your from email address is "from@hotmail.com" then host should be "smtp.hotmail.com"**
+                            smtpClient.UseDefaultCredentials = true;
+                            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                            smtpClient.EnableSsl = true;
+                            smtpClient.Send(msg);
+                            Console.WriteLine(itm.Title + " Email Sended Successfully");
+                        }
 
-                    AddArticle(itm.Guid);
+                        AddArticle(itm.Guid);
+                    }
+                    Console.Clear();
+                    Console.WriteLine("Scanned at " + DateTime.Now.ToString());
+                    System.Threading.Thread.Sleep(300000);
                 }
-                Console.Clear();
-                Console.WriteLine("Scanned at " + DateTime.Now.ToString());
-                System.Threading.Thread.Sleep(1500);
+                catch
+                {
+
+                }
+                
             }
 
 
@@ -59,22 +67,13 @@ namespace crypto_analyse_newsletter
                 return false;
             }
         }
-        static List<string> GetArticlesHash()
+        static string GetArticlesHash()
         {
             if (!System.IO.File.Exists("db.txt"))
             {
-                return new List<string>();
+                return "";
             }
-            System.IO.StreamReader sr = new System.IO.StreamReader("db.txt");
-
-            List<string> list = new List<string>();
-            string line;
-            while ((line = sr.ReadLine()) != null)
-            {
-                list.Add(line);
-            }
-            sr.Close();
-            return list;
+            return System.IO.File.ReadAllText("db.txt"); 
         }
     }
 }
