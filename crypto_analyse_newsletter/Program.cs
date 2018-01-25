be920dc6b0d4543900f25f93d204df9830dc40aa
AzzRun <qmartine@student.42.fr>
2018-01-25 19:19:31 +0100

    crontab optimisation

--- crypto_analyse_newsletter/Program.cs
+++ crypto_analyse_newsletter/Program.cs
@@ -13,55 +13,51 @@ namespace crypto_analyse_newsletter
         {
             Console.WriteLine("Crypto-Analyse Push service starting..");
             FeedParser feedParser = new FeedParser();
-            while (true)
+
+            try
             {
-                try
+                string ArticleKnowed = GetArticlesHash();
+                IList<Item> items = feedParser.Parse("https://crypto-analyse.org/category/accueil/analyses/feed/", FeedType.RSS).Where(x => ArticleKnowed.Contains(x.Guid) == false).ToList();
+                foreach (var itm in items)
                 {
-                    string ArticleKnowed = GetArticlesHash();
-                    IList<Item> items = feedParser.Parse("https://crypto-analyse.org/category/accueil/analyses/feed/", FeedType.RSS).Where(x => ArticleKnowed.Contains(x.Guid) == false).ToList();
-                    foreach (var itm in items)
+                    using (WebClient wc = new WebClient())
                     {
-                        using (WebClient wc = new WebClient())
+                        string htmlString = wc.DownloadString(itm.Link);
+                        HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+                        htmlDocument.LoadHtml(htmlString);
+                        var text = htmlDocument.DocumentNode.SelectSingleNode("//*[@id='off-canvas-body']/div[4]/div/div/div/div[1]/div/article").InnerHtml;
+                        var msg = new MailMessage("quentin.martinez@outlook.com", "azrunsoft@gmail.com", itm.Title, text);
+                        msg.To.Add("quentin.martinez@outlook.com");
+                        msg.To.Add("bc@chunkz.net");
+                        msg.IsBodyHtml = true;
+                        var login = GetLogin();
+                        var smtp = new SmtpClient
                         {
-                            string htmlString = wc.DownloadString(itm.Link);
-                            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
-                            htmlDocument.LoadHtml(htmlString);
-                            var text = htmlDocument.DocumentNode.SelectSingleNode("//*[@id='off-canvas-body']/div[4]/div/div/div/div[1]/div/article").InnerHtml;
-                            var msg = new MailMessage("quentin.martinez@outlook.com", "azrunsoft@gmail.com", itm.Title, text);
-                            msg.To.Add("quentin.martinez@outlook.com");
-                            msg.To.Add("bc@chunkz.net");
-                            msg.IsBodyHtml = true;
-                            var login = GetLogin();
-                            var smtp = new SmtpClient
-                            {
-                                Host = "smtp.gmail.com",
-                                Port = 587,
-                                EnableSsl = true,
-                                DeliveryMethod = SmtpDeliveryMethod.Network,
-                                UseDefaultCredentials = false,
-                                Credentials = new NetworkCredential(login.Split(':')[0], login.Split(':')[1])
-                            };
-                            smtp.Send(msg);
-                            Console.WriteLine(itm.Title + " Email Sended Successfully");
-                        }
-
-                        AddArticle(itm.Guid);
+                            Host = "smtp.gmail.com",
+                            Port = 587,
+                            EnableSsl = true,
+                            DeliveryMethod = SmtpDeliveryMethod.Network,
+                            UseDefaultCredentials = false,
+                            Credentials = new NetworkCredential(login.Split(':')[0], login.Split(':')[1])
+                        };
+                        smtp.Send(msg);
+                        Console.WriteLine(itm.Title + " Email Sended Successfully");
+                        System.IO.File.AppendAllText("log.txt", itm.Title + " Email Sended Successfully");
                     }
-                    Console.Clear();
-                    Console.WriteLine("Scanned at " + DateTime.Now.ToString());
-                    System.Threading.Thread.Sleep(300000);
+
+                    AddArticle(itm.Guid);
                 }
-                catch(Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                
+                Console.Clear();
+                Console.WriteLine("Scanned at " + DateTime.Now.ToString());
+                System.IO.File.AppendAllText("log.txt", "Scanned at " + DateTime.Now.ToString());
+
             }
-
-
-
-
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("log.txt", ex.Message);
+            }
         }
+
         static bool AddArticle(string hash)
         {
             try
@@ -80,13 +76,13 @@ namespace crypto_analyse_newsletter
             {
                 return "";
             }
-            return System.IO.File.ReadAllText("db.txt"); 
+            return System.IO.File.ReadAllText("db.txt");
         }
         static string GetLogin()
         {
-            if (!System.IO.File.Exists("db.txt"))
+            if (!System.IO.File.Exists("login.txt"))
             {
-                return "";
+                return "email@gmail.com:pass";
             }
             return System.IO.File.ReadAllText("login.txt");
         }
